c66d58b37b2346fc07b1de20652f598f184eb511
Kim Iversen <kim@wwan.no>
2021-07-01 00:01:06 +0200

    Changed example to use Terraform registry

--- examples/website-backend/main.tf
+++ examples/website-backend/main.tf
@@ -3,7 +3,8 @@ provider "azurerm" {
 }
 
 module "azure-backend" {
-  source = "../../"
+  source  = "app.terraform.io/b24x7/terraform-backend/azurerm"
+  version = "0.0.1-dev"
   backend_resource_group_name = "rg-terraform"
   environments = ["dev", "test", "prod"]
   project_name = "website"
