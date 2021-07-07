eb6070f7e1923222114174a17445777c357d335e
Kim Iversen <kim.iversen@blinq.no>
2021-07-07 15:41:01 +0200

    Removed azuredevops integration

--- examples/website-backend/main.tf
+++ examples/website-backend/main.tf
@@ -8,15 +8,14 @@ provider "azurerm" {
 
 module "azure-backend" {
   source  = "../../"
-  #version = "0.0.1-dev"
   backend_resource_group_name = "rg-terraform"
   environments = ["dev"]
   project_name = "kportal"
 
-  enable_azuredevops = true
-  azuredevops = {
-    project_name = "Kundeportal"
-    project_description = "Managed by Terraform"
-    visibility = "private"
-  }
+  # enable_azuredevops = false
+  # azuredevops = {
+  #   project_name = "Kundeportal"
+  #   project_description = "Managed by Terraform"
+  #   visibility = "private"
+  # }
 }
--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "2.65.0"
     }
-    azuredevops = {
-      source = "microsoft/azuredevops"
-      version = "0.1.5"
-    }
+    #azuredevops = {
+    #  source = "microsoft/azuredevops"
+    #  version = "0.1.5"
+    #}
   }
 }
 
